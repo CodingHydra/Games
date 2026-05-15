98cbfe1c666634acec9fbed052c0964863f571e7
Bishal Gaire <bgaire01@qub.ac.uk>
2026-05-15 22:09:06 +0100

    learning raylib

--- Raylib-CPP-Starter-Template-for-VSCODE-V2-main/Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/main.cpp
+++ Raylib-CPP-Starter-Template-for-VSCODE-V2-main/Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/main.cpp
@@ -1,38 +1,110 @@
 #include <raylib.h>
+#include <iostream>
+
+class Ball
+{
+    public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
+
+
+    void Draw()
+    {
+       DrawCircle(x, y, radius, WHITE);  
+    }
+
+    void Update()
+    {
+        x += speed_x;
+        y += speed_y;
+
+        if(y + radius >= GetScreenHeight() || y - radius <= 0)
+        {
+            speed_y *= -1;
+        }
+        if(x + radius >= GetScreenWidth() || x - radius <= 0)
+        {
+            speed_x *= -1;
+        }
+
+
+    }
+};
+
+class Paddle
+{
+public:
+float x , y;
+float width, height;
+int speed;
+
+void Draw()
+{
+    DrawRectangle(x, y, width, height, WHITE);
+
+
+};
+
+void Update()
+{
+    if(IsKeyDown(KEY_UP))
+    {
+        y = y - speed;
+    }
+    if(IsKeyDown(KEY_DOWN))
+    {
+        y = y + speed;
+    }
+}
+
+};
+Paddle player;
+Ball ball;
+
 
 int main()
 {
-    int ballX = 400;
-    int ballY = 400;
-    Color green = {20, 160, 133, 255};
+    std::cout << "The Complier can complie" << std::endl;
+    const int screen_height = 800;
+    const int screen_width = 1200;
 
-
-    InitWindow(800,800,"My first game");
+    InitWindow(screen_width, screen_height, " Pong Game");
     SetTargetFPS(60);
-    //Game Loop
+
+    ball.radius = 20;
+    ball.x = screen_width / 2;
+    ball.y = screen_width / 2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - screen_width - 10;
+    player.y = screen_height / 2 - player.height / 2;
+    player.speed = 6; 
+
+
+
     while(WindowShouldClose() == false)
     {
-        //Event Handling
-        if(IsKeyDown(KEY_RIGHT))
-        {
-            ballX += 3;
-        } else if (IsKeyDown(KEY_LEFT))
-        {
-            ballX -= 3;
-        }
+        BeginDrawing();
 
-        //Updating Postition
-        
+        //Updating
+        ball.Update();
+        player.Update();
 
 
         //Drawing
-        BeginDrawing();
-        ClearBackground(green);
-        DrawCircle(ballX, ballY, 20, WHITE);
+        ClearBackground(BLACK);
+        DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
+        ball.Draw(); 
+        
+        player.Draw();
+        DrawRectangle(screen_width -35, screen_height /2 -60, 25, 120, WHITE);
 
         EndDrawing();
     }
-
 
     CloseWindow();
     return 0;
